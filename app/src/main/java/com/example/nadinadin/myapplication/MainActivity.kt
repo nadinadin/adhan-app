282bee0ed4dcb6547e2aa03ed3fcbb1c6b3bcd72
Tristan Mc Leay <tristan.mcleay@ivt.com.au>
2018-01-27 14:53:15 +0700

    remove unused

--- app/src/main/java/com/example/nadinadin/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/nadinadin/myapplication/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.nadinadin.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.GridView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
